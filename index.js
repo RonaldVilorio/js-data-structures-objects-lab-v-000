48eff2af8819bf3fda9216b47e3887a6c4b138fe
ronald vilorio <soccer23415@gmail.com>
2018-05-13 16:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value){
-
-
+  const new_driver = {...driver}
+  new_driver[key] = value
+  return new_driver
 
 }
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
