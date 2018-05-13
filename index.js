a3023748d96019a6e047e2eef48336d219035664
ronald vilorio <soccer23415@gmail.com>
2018-05-13 16:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromDriverByKey(driver, key){
   return delete driver.key
 }
 function destructivelyDeleteFromDriverByKey(driver, key){
-  driver = delete driver.key
-   return driver
+  delete driver.key
+  return driver
 }
